ecbca03574be3729f1e1c2966f86d1c3875c4db1
Dzianis Shutikov <Dzianis_Shutikov@epam.com>
2018-03-01 01:17:40 +0300

    tmp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency > 10000 ) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    const currencies = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+
+    let currentCurrency = currency;
+
+    const result = {};
+
+    for (let key in currencies) {
+        if(currentCurrency >= currencies[key]) {
+            const value = Math.floor(currentCurrency / currencies[key]);
+            result[key] = value;
+            currentCurrency -= value * currencies[key];
+        }
+    }
+
+    return result;
 }
